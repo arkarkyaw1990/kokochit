2d20b81c43cad5a5602e1a6f6a10872eb06493b2
Arkar Kyaw <arkarkyaw1990@gmail.com>
2018-05-21 16:52:23 +0630

    separate logout

--- app/Http/Controllers/Auth/AdminLoginController.php
+++ app/Http/Controllers/Auth/AdminLoginController.php
@@ -10,7 +10,7 @@ class AdminLoginController extends Controller
 {
 	public function __construct()
     {
-        $this->middleware('guest:admin');
+        $this->middleware('guest:admin', ['except' => ['logout']]);
     }
     public function showLoginForm()
     {
@@ -29,5 +29,10 @@ class AdminLoginController extends Controller
     	}
 
     	return redirect()->back()->withInput($request->only('email', 'remember'));
+    }
+    public function logout()
+    {
+        Auth::guard('admin')->logout();
+        return redirect('/');
     }
 }
